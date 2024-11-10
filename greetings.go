ad29ab4b0ada0bdace7e371d3e806044f228c5a2
Gael Rozario <gaelrozario@gmail.com>
2024-11-10 14:04:02 +0530

    fmt

--- greetings.go
+++ greetings.go
@@ -6,11 +6,12 @@ import (
 	"time"
 )
 
-func Greet(name string) (string,error) {
+func Greet(name string) (string, error) {
 
-    if name != ""{
-        return "",errors.New("name should not be empty")
-    }
+	if name != "" {
+		return "", errors.New("name should not be empty")
+	}
 	message := fmt.Sprintf("Hi there %s! How are you doing\nHappy %s", name, time.Now().Weekday())
-	return message,nil
+
+	return message, nil
 }
