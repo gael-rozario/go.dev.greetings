0b3303fa366ca525fc6a829257ea51a3a9c38c2f
Gael Rozario <gaelrozario@gmail.com>
2024-11-10 18:10:33 +0530

    update greetings function

--- greetings.go
+++ greetings.go
@@ -19,18 +19,16 @@ func Greet(name string) (string, error) {
 
 func Greetings(name []string) (map[string]string, error) {
 
-    greetings := make(map[string]string)
+	greetings := make(map[string]string)
 
-    for i:= range name{
-        if name[i] == ""{
-            return nil, errors.New("Name cannot be empty")
-        }
-    }
-
-    for i:= range name{
-       greetings[name[i]]= fmt.Sprintf(get_greeting_format(),name[i])
-    }
-    return greetings,nil
+	for i := range name {
+		greeting, err := Greet(name[i])
+		if err != nil {
+			return nil,errors.New("name should not be empty")
+		}
+        greetings[name[i]] = greeting
+	}
+	return greetings, nil
 }
 
 func get_greeting_format() string {
