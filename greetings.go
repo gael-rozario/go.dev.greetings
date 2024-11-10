be3f53412ce760a669c390486be2b82ca573de7e
Gael Rozario <gaelrozario@gmail.com>
2024-11-10 14:02:51 +0530

    adding error handling

--- greetings.go
+++ greetings.go
@@ -1,11 +1,16 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
 
-func Greet(name string) string {
+func Greet(name string) (string,error) {
+
+    if name != ""{
+        return "",errors.New("name should not be empty")
+    }
 	message := fmt.Sprintf("Hi there %s! How are you doing\nHappy %s", name, time.Now().Weekday())
-	return message
+	return message,nil
 }
