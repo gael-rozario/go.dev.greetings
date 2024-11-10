a2e4afa4773b120f5d78eef6d62cef6b7f5d8a4f
Gael Rozario <gaelrozario@gmail.com>
2024-11-10 15:00:59 +0530

    adding random greetings

--- greetings.go
+++ greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -11,7 +12,18 @@ func Greet(name string) (string, error) {
 	if name != "" {
 		return "", errors.New("name should not be empty")
 	}
-	message := fmt.Sprintf("Hi there %s! How are you doing\nHappy %s", name, time.Now().Weekday())
+	message := fmt.Sprintf(get_greeting_format(), name)
 
 	return message, nil
+}
+
+func get_greeting_format() string {
+
+	formatlist := []string{
+		"Hi there %s have a great day",
+		"How you doing %s",
+		fmt.Sprintf("Hi there %%s have a great %s", time.Now().Day()),
+		"Hello %s welcom to go programming ",
+	}
+	return formatlist[rand.Intn(len(formatlist))]
 }
